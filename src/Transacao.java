14d30abb87dcbb45b434767b018250d88c44f01c
johann0301 <johann.bezerra27@gmail.com>
2025-11-16 13:49:56 -0300

    subindo pequenas mudanças para merge

--- src/Transacao.java
+++ src/Transacao.java
@@ -1,9 +1,4 @@
-// Arquivo: Transacao.java
 
-/*
- * Esta é a classe "molde". Ela não faz nada sozinha,
- * apenas armazena os dados de UMA transação.
- */
 public class Transacao {
 
     private double valor;
@@ -21,23 +16,18 @@ public class Transacao {
         this.descricao = descricao;
     }
 
-    // --- Getters e Setters ---
-    // Getters são usados para LER os valores
     public double getValor() { return valor; }
     public String getTipo() { return tipo; }
     public String getCategoria() { return categoria; }
     public String getData() { return data; }
     public String getDescricao() { return descricao; }
 
-    // Setters são usados para EDITAR os valores
     public void setValor(double valor) { this.valor = valor; }
     public void setTipo(String tipo) { this.tipo = tipo; }
     public void setCategoria(String categoria) { this.categoria = categoria; }
     public void setData(String data) { this.data = data; }
     public void setDescricao(String descricao) { this.descricao = descricao; }
 
-    // --- Método toString ---
-    // Isso define como a transação será impressa no console.
     @Override
     public String toString() {
         // O "R$ %.2f" formata o valor para 2 casas decimais
